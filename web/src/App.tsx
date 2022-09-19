026942e04fdd176339df365cd0fc6cce32249cda
mario <mario.roberto1@hotmail.com>
2022-09-18 22:23:26 -0300

    ok

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -20,7 +20,7 @@ function App() {
     <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
       <img src={logoImg} alt=""></img>
       <h1 className="text-6xl text-white font-black mt-20">Seu <span className='text-transparent bg-nlw-gradient bg-clip-text'>duo</span> está aqui.</h1>
-
+      <h1>start  the commit</h1>
 
       <div className="grid grid-cols-6 gap-6 mt-16">
 
